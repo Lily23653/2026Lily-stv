83acf5d75ba7895785de19392aec96270d0ab608
Lily23653 <23653@burnside.school.nz>
2026-08-21 16:21:45 +1200

    Specific pages for season filter added

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def hello_world():
     sql= "SELECT * FROM fishing;"
     items = query_db(sql)
     return render_template("home.html", items=items)
-    
+
+#Main page for all fishing skills
 @app.route("/fishing")
 def fishing():
     sql= "SELECT * FROM fishing;"
@@ -46,6 +47,7 @@ def fish_detail(id):
         abort(404)
     return render_template("fish.html", fish=fish)
 
+#Main page for all planting information
 @app.route("/planting")
 def planting():
     sql= "SELECT * FROM planting;"
@@ -62,12 +64,14 @@ def plant_detail(id):
         abort(404)
     return render_template("plant.html", plant=plant)
 
+#Main page for all Farm
 @app.route("/Farm")
 def farm_list():
     sql= "SELECT * FROM Farm;"
     farm = query_db(sql)
     return render_template("Farm.html",farm=farm)
 
+# Going to a specific farm layout page
 @app.route("/Farm/<int:id>")
 def farm_detail(id):
     sql = """
@@ -77,12 +81,14 @@ def farm_detail(id):
         abort(404)
     return render_template("FarmLayout.html", farm=farm)
 
+#Main page for all NPC
 @app.route("/NPC")
 def npc_list():
     sql= "SELECT * FROM NPC;"
     npc = query_db(sql)
     return render_template("NPC.html",NPC=npc)
 
+# Going to a specific NPC detail page
 @app.route("/NPC/<int:id>")
 def npc_detail(id):
     sql = """
@@ -95,21 +101,24 @@ def npc_detail(id):
 #filter content
 @app.route("/season/<season_name>")
 def season_filter(season_name):
-    #searching for specofoc season and including "All" and "Any"
+#searching for specofoc season and including "All" and "Any"
+    term = f"%{season_name}%"
     sql_fish = """
     SELECT * FROM fishing 
-    WHERE season LIKE ? OR Season LIKE '%All%' OR Season LIKE '%Any%';
+    WHERE season LIKE ? OR season LIKE '%All%' OR season LIKE '%Any%';
     """
-    fishes = query_db(sql_fish, ("%" + season_name + "%"))
+    fishes = query_db(sql_fish, (term,))
 
-    #search in planting
+#search in planting
     sql_plant = """
     SELECT * FROM planting 
-    WHERE season LIKE ? OR Season LIKE '%All%' OR Season LIKE '%Any%';
+    WHERE season LIKE ? OR season LIKE '%All%' OR season LIKE '%Any%';
     """
-    plants = query_db(sql_plant, ("%" + season_name + "%"))
+    plants = query_db(sql_plant, (term,))
+    
     return render_template("season.html", season=season_name, fishes=fishes, plants=plants)
 
+#Search bar section
 @app.route('/fish', methods=['GET'])
 def search_for_fish():
     query = request.args.get('q', "").strip()[:50]
@@ -135,14 +144,12 @@ def search_for_fish():
     return render_template('fish_list.html',fish_list=fish_list, query=query, selected_season=season_item)
 
 #If the entered web address is wrong
-@app.errorhandler(404)
+@app.errorhandler(404)#When the error is 404
 def page_not_found(e):
-    #When the error is 404
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
+@app.errorhandler(500)#When the inner system went wrong (500)
 def internal_server_error(e):
-    #When the inner system went wrong (500)
     return render_template('500.html'), 500
 
 
